c442d41cbf3c815367dbd6a8b43c3fda248b3ef8
Ssssv11 <1416431087@qq.com>
2022-08-30 16:40:11 +0800

    LeetCode 33

---
+++ Array/33.搜索旋转排序数组.java
@@ -0,0 +1,35 @@
+package Array;
+/*
+ * @lc app=leetcode.cn id=33 lang=java
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } 
+            if(nums[mid] >= nums[left]) {
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if(target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return - 1;
+    }
+}
+// @lc code=end
+
