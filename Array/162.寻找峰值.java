e121f5b5db67ac195302ae12026ed6d7b88ac821
Ssssv11 <1416431087@qq.com>
2022-08-30 11:10:24 +0800

    LeetCode 162

---
+++ Array/162.寻找峰值.java
@@ -0,0 +1,25 @@
+package Array;
+
+/*
+ * @lc app=leetcode.cn id=162 lang=java
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+// @lc code=end
+
