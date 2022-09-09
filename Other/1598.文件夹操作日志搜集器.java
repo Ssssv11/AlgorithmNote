321278e7f90a8fb8ed84bcad53937d58a51756b9
Ssssv11 <1416431087@qq.com>
2022-09-09 19:46:01 +0800

    LeetCode 2022/09/09 #1598

---
+++ Other/1598.文件夹操作日志搜集器.java
@@ -0,0 +1,39 @@
+package Other;
+/*
+ * @lc app=leetcode.cn id=1598 lang=java
+ *
+ * [1598] 文件夹操作日志搜集器
+ */
+
+// @lc code=start
+class Solution {
+    public int minOperations(String[] logs) {
+        int res = 0;
+        for (int i = 0; i < logs.length; i++) {
+            String log = logs[i];
+            switch (log) {
+                case "./":
+                    break;
+                case "../":
+                    res = res >= 1 ? res - 1 : res;
+                    break;
+                default:
+                    res++;
+            }
+
+            // if(logs[i].equals("./")) {
+                
+            // } else if(logs[i].equals("../")) {
+            //     res = res >= 1 ? res-1 : res;
+            //     // if(res >= 1) {
+            //     //     res--;
+            //     // }
+            // } else {
+            //     res++;
+            // }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
